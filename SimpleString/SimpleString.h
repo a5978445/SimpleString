0422fd37c5e7d6e2ee4f1aaafcaa2f032bcaabb7
fitch <510491354@qq.com>
2019-05-08 16:51:40 +0800

    Initial Commit

---
+++ SimpleString/SimpleString.h
@@ -0,0 +1,19 @@
+//
+//  SimpleString.h
+//  SimpleString
+//
+//  Created by 李腾芳 on 2019/5/8.
+//  Copyright © 2019 HSBC Holdings plc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleString.
+FOUNDATION_EXPORT double SimpleStringVersionNumber;
+
+//! Project version string for SimpleString.
+FOUNDATION_EXPORT const unsigned char SimpleStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleString/PublicHeader.h>
+
+
